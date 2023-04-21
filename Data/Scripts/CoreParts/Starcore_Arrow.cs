07be28bec2ade5a46a7029fd5ce536a4d640bcd4
InvalidArgument3 <51190031+InvalidArgument3@users.noreply.github.com>
2023-04-21 04:20:03 -0500

    heat as a reload indicator

--- Data/Scripts/CoreParts/Starcore_Arrow.cs
+++ Data/Scripts/CoreParts/Starcore_Arrow.cs
@@ -132,10 +132,10 @@ namespace Scripts {
                     ReloadTime = 300, // Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
                     MagsToLoad = 1, // Number of physical magazines to consume on reload.
                     DelayUntilFire = 1, // How long the weapon waits before shooting after being told to fire. Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
-                    HeatPerShot = 10, // Heat generated per shot.
-                    MaxHeat = 85, // Max heat before weapon enters cooldown (70% of max heat).
+                    HeatPerShot = 1, // Heat generated per shot.
+                    MaxHeat = 20, // Max heat before weapon enters cooldown (70% of max heat).
                     Cooldown = 0, // Percentage of max heat to be under to start firing again after overheat; accepts 0 - 0.95
-                    HeatSinkRate = 10, // Amount of heat lost per second.
+                    HeatSinkRate = 0, // Amount of heat lost per second.
                     DegradeRof = false, // Progressively lower rate of fire when over 80% heat threshold (80% of max heat).
                     ShotsInBurst = 0, // Use this if you don't want the weapon to fire an entire physical magazine in one go. Should not be more than your magazine capacity.
                     DelayAfterBurst = 0, // How long to spend "reloading" after each burst. Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
