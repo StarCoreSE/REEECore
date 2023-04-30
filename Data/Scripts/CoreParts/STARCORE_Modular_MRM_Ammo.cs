9e8e7cb17e14153956072c1306cc41c4b267b512
InvalidArgument3 <51190031+InvalidArgument3@users.noreply.github.com>
2023-04-30 02:54:22 -0500

    wow! more MRM damage!

--- Data/Scripts/CoreParts/STARCORE_Modular_MRM_Ammo.cs
+++ Data/Scripts/CoreParts/STARCORE_Modular_MRM_Ammo.cs
@@ -626,7 +626,7 @@ namespace Scripts
                 {
                     Enable = true,
                     Radius = 5f, // Radius of AOE effect, in meters.
-                    Damage = 65000f,
+                    Damage = 130000f,
                     Depth = 5f, // Max depth of AOE effect, in meters. 0=disabled, and AOE effect will reach to a depth of the radius value
                     MaxAbsorb = 0, // Soft cutoff for damage, except for pooled falloff.  If pooled falloff, limits max damage per block.
                     Falloff = Pooled, //.NoFalloff applies the same damage to all blocks in radius
